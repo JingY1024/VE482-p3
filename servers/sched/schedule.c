154b4afb6d718401b991aacb93ed3b785af53997
Krystor <krystor@sjtu.edu.cn>
2017-12-15 12:20:10 +0800

    need to debug

--- servers/sched/schedule.c
+++ servers/sched/schedule.c
@@ -532,12 +532,11 @@ int lottery(void)
 
     if (!ticketNum)
     {
-        printf("ticket number: %d\n",ticketNum);
         return OK;
     }
 
     pick = random()%ticketNum;
-    printf("The picked lottery is: %d, ticket number is: %d\n",pick,ticketNum);
+    printf("Lottery picked: %d. Lottery total: %d\n",pick,ticketNum);
     current=0;
     for (i=0,rmp=schedproc;i<NR_PROCS;i++,rmp++)
     {
